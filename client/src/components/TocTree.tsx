88708439cca6643ac0720b7ad1c9fd10b9cc49b4
lz <joemonkeylee@gmail.com>
2026-09-06 22:25:09 +0800

    style(client): refine page thumbnail spacing

--- client/src/components/TocTree.tsx
+++ client/src/components/TocTree.tsx
@@ -71,7 +71,7 @@ export default function TocTree({ toc, currentPage, totalPages, storagePath, onP
             ))}
           </div>
         ) : (
-          <div className="p-3 space-y-3" ref={thumbRef}>
+          <div className="p-4 space-y-4" ref={thumbRef}>
             {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
               <div
                 key={page}
